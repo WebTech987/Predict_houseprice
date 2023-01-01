c2a18149ad4bfaba58400ac4f54e8d9463224c5e
Suraj Ahirwar <surajahirwar3686@gmail.com>
2023-01-02 03:09:13 +0530

    This commit is to update html and app file

--- app.py
+++ app.py
@@ -26,9 +26,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    scalerfile = 'scaling.pkl'
+    scalar = pickle.load(open(scalerfile, 'rb'))
+    final_input=scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="This is the predicted houseprice value {}".format(output))
+
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
-
-
-
-
